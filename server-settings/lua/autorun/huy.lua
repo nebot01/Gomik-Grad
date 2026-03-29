9adecacfb68b0acf702c25ea5edbf41fa2fbcd94
Ne bot01 <darkpuro.little.brother@gmail.com>
2026-03-29 09:15:59 +0800

    Update huy.lua

--- server-settings/lua/autorun/huy.lua
+++ server-settings/lua/autorun/huy.lua
@@ -2,6 +2,6 @@ if SERVER then
    RunConsoleCommand("hostname", "Gomik-Grad")
    RunConsoleCommand("sv_location", "RU")
    RunConsoleCommand("sv_lan", "0")
-   RunConsoleCommand("sv_loadingurl", "https://nebot01.github.io/Local-box-loading-screen/")
+   RunConsoleCommand("sv_loadingurl", "https://nebot01.github.io/Gomik-Grad-loading/")
    RunConsoleCommand("bot_zombie", "1")
 end
